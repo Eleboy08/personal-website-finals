3a9c9846b5c480aa4ddd984d7f2fd3d58c1b251c
Eleboy08 <hmmacasaettobias@student.apc.edu.ph>
2026-02-23 12:56:57 +0000

    rfg

--- my-portfolio-project/backend/app.py
+++ my-portfolio-project/backend/app.py
@@ -1,13 +1,14 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from supabase import create_client, Client
 
 app = Flask(__name__)
-CORS(app) # Crucial for your Vue frontend to talk to this
+CORS(app)
 
-# Your actual Supabase details
-SUPABASE_URL = "https://kowqprybqprnrutcsfuk.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtvd3FwcnlicXBybnJ1dGNzZnVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4Mzk2MzQsImV4cCI6MjA4NzQxNTYzNH0.P8QqFv6S1OLhyZ9OaYJKaKmY0zsQoCwVuqT_e0xtPGg"
+# This pulls the "secrets" from your Vercel Settings
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
@@ -23,12 +24,8 @@ def get_comments():
 def add_comment():
     try:
         data = request.get_json()
-        # Ensure your table has 'name' and 'message' columns!
         new_comment = {"name": data.get('name'), "message": data.get('message')}
         response = supabase.table('guestbook').insert(new_comment).execute()
         return jsonify(response.data), 201
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
